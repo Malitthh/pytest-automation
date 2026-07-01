4df20aaf296ddb5232e4c6ae49484e621a3752a2
Malith <malith.senadheera@cube.global>
2026-07-01 13:48:20 +0530

    updated base_test.py

--- src/common/base/base_test.py
+++ src/common/base/base_test.py
@@ -1,6 +1,9 @@
+from playwright.sync_api import Page
 import pytest
 
 class BaseTest:
     @pytest.fixture(autouse=True)
-    def setup(self, page: "Page"):
-        self.page = page
+    def setup(self, page: Page):
+        self.page = page 
+        yield
+        page.context.clear_cookies()
