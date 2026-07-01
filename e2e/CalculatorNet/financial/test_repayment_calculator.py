ca2102eeb462fafeb48a92761c9f85df31ef4fff
Malith <malith.senadheera@cube.global>
2026-07-01 19:01:36 +0530

    updated base_test.py

--- e2e/CalculatorNet/financial/test_repayment_calculator.py
+++ e2e/CalculatorNet/financial/test_repayment_calculator.py
@@ -6,6 +6,7 @@ from common.utils.data_loader import load_test_data
 _DATA = load_test_data("CalculatorNet","financial", "repayment_calculator_data.json")
 VALID_CASES = _DATA["valid_cases"]
 FIXED_INSTALLMENT = _DATA["fixed_installments"]
+MONTHS_ONLY_CASES = _DATA["months_only_cases"]
 FIXED_BLANK = [FIXED_INSTALLMENT["all_blank"]]
 
 @pytest.mark.calculatornet
@@ -53,7 +54,18 @@ class TestRepaymentPositiveCases(BaseTest):
         )
         expected = scenario["expected_interest"]
         expect(repayment_page.total_interest_cell(expected)).to_be_visible()
-        
+
+    @pytest.mark.parametrize("scenario", MONTHS_ONLY_CASES, ids=[s["id"] for s in MONTHS_ONLY_CASES])
+    def test_monthly_payment_for_months_only_term(self, repayment_page, scenario):
+        repayment_page.fixed_time_calculate(
+            scenario["loan_amount"],
+            scenario["annual_interest_rate"],
+            scenario["years"],
+            scenario["months"]
+        )
+        expected = scenario["expected_monthly"]
+        expect(repayment_page.monthly_payment_cell(expected)).to_be_visible()
+
     @pytest.mark.parametrize("scenario", FIXED_BLANK, ids=[s["id"] for s in FIXED_BLANK])
     def test_fixed_installment_all_blank_shows_errors(self, repayment_page, scenario):
         repayment_page.fixed_installment_calculate(
